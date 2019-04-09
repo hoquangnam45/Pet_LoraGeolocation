1e9e80eba6d844d96a11849b7906ade7fc8fd10d
Ho Quang Nam <hoquangnam45@gmail.com>
2019-04-10 01:59:00 +0700

    Really remove build folder

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-/home/hoquangnam/Arduino/Project_HTN/build/core/libb64/cdecode.c.o: \
- /home/hoquangnam/.arduino15/packages/Heltec-esp32/hardware/esp32/0.0.2-rc1/cores/esp32/libb64/cdecode.c \
- /home/hoquangnam/.arduino15/packages/Heltec-esp32/hardware/esp32/0.0.2-rc1/cores/esp32/libb64/cdecode.h \
- /home/hoquangnam/.arduino15/packages/Heltec-esp32/hardware/esp32/0.0.2-rc1/tools/sdk/include/newlib/stdint.h \
- /home/hoquangnam/.arduino15/packages/Heltec-esp32/hardware/esp32/0.0.2-rc1/tools/sdk/include/newlib/machine/_default_types.h \
- /home/hoquangnam/.arduino15/packages/Heltec-esp32/hardware/esp32/0.0.2-rc1/tools/sdk/include/newlib/sys/features.h \
- /home/hoquangnam/.arduino15/packages/Heltec-esp32/hardware/esp32/0.0.2-rc1/tools/sdk/include/newlib/sys/_intsup.h
